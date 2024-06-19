4234058e3c3a2dd09f077ffef428b18346154929
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-19 17:27:56 +0900

    module os

---
+++ modules/os.js
@@ -0,0 +1,20 @@
+// oparation system
+const os = require("os")
+
+// platform
+// console.log(os.platform())
+
+// information
+// console.log(os.cpus())
+
+// architecture
+// console.log(os.arch())
+
+// all memory
+// console.log(os.freemem())
+
+// available memory
+// console.log(os.totalmem())
+
+// entry point
+console.log(os.homedir())
