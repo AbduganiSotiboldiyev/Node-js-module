7eb1b237fb8b0bcb905b3edad9873fea0e1496f6
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-19 18:37:55 +0900

    http modules

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+
+    res.write("<h1> Salom Abdugani Never give up </h1>")
+    res.end()
+})
+
+server.listen(3000, () =>{
+    console.log("Server has been started on port :3000")
+})
