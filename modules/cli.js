1de26c7370457f18711cee617ee8f47d72b9bf49
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-22 20:35:41 +0900

    cli

---
+++ modules/cli.js
@@ -0,0 +1,12 @@
+const logger = () => {
+    const response = {}
+    for (i = 2; i< process.argv.length; i++) {
+        const arg = process.argv[i].split("=")
+        response[arg[0]] = arg[1] ? arg[1] : true
+    }
+
+    return response
+}
+
+
+console.log(logger())
