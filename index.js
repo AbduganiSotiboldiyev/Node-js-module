d9a4101c0d7204944705f3c373026fa66c60b2df
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-22 19:46:52 +0900

    workin with html

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require("http")
+const fs = require('fs')
+const path = require("path")
 
 
 // base information
@@ -19,14 +21,28 @@ const http = require("http")
 
 const server = http.createServer((req,res) =>{
     if(req.method === "GET") {
-        res.writeHead(200 , {"Content-Type" : "text/html"})
-        res.end(`
-            <h2> Send Email</h2>
-            <form method = "post" action = "/" >
-                <input name="email" type ="email" placeholder= "Enter your email">
-                <button type = "submit">Send  email </button>
-            </form>
-        `)
+        // res.writeHead(200 , {"Content-Type" : "text/html"})
+
+        if(req.url === "/"){
+            fs.readFile(path.join(__dirname, "templates" , "index.html"), "utf-8" , (err,content) => {
+                if(err) throw new err
+
+                res.end(content)
+            })
+        }else if(req.url ==="/about") {
+            fs.readFile(path.join(__dirname, "templates" , "about.html"), "utf-8" , (err,content) => {
+                if(err) throw new err
+
+                res.end(content)
+            })
+        }else if(req.url ==="/contact") {
+            fs.readFile(path.join(__dirname, "templates" , "contacts.html"), "utf-8" , (err,content) => {
+                if(err) throw new err
+
+                res.end(content)
+            })
+        }
+
     } else if(req.method === "POST") {
 
         const email = []
